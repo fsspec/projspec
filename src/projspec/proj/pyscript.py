45a48b5a4fefc7147a694bc346e285bfa9cdcbfa
Martin Durant <martin.durant@alumni.utoronto.ca>
2025-07-10 13:26:06 -0400

    Add minimal pyscript

---
+++ src/projspec/proj/pyscript.py
@@ -0,0 +1,26 @@
+import toml
+
+from projspec.proj import ProjectSpec
+from projspec.utils import AttrDict
+
+
+class PyScriptSpec(ProjectSpec):
+    def match(self) -> bool:
+        basenames = [_.rsplit("/", 1)[0] for _ in self.root.filelist]
+        return "pyscript.toml" in basenames or "pyscript.json" in basenames
+
+    def parse(self) -> None:
+        try:
+            with self.root.fs.open(f"{self.root.url}/pyscript.toml") as f:
+                meta = toml.load(f)
+        except FileNotFoundError:
+            with self.root.fs.open(f"{self.root.url}/pyscript.json") as f:
+                meta = toml.load(f)
+        cont = AttrDict()
+        if "packages" in meta:
+            cont["environment"] = AttrDict(default=meta["packages"])
+        self._contents = cont
+
+        # perhaps a local deployment can be a reasonable artifact
+        # https://github.com/pyscript/pyscript-cli
+        self._artifacts = AttrDict()
