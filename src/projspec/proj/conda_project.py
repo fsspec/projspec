07f687a9a4e021fe40977ef1df493090823def94
Martin Durant <martin.durant@alumni.utoronto.ca>
2025-07-21 17:28:26 -0400

    start conda-project

--- src/projspec/proj/conda_project.py
+++ src/projspec/proj/conda_project.py
@@ -1,5 +1,8 @@
 from projspec.proj import ProjectSpec
-from projspec.utils import _yaml_no_jinja
+from projspec.utils import AttrDict, _yaml_no_jinja
+
+# lockfile format is as produced by conda-lock
+# https://github.com/conda/conda-lock
 
 
 class CondaProject(ProjectSpec):
@@ -7,20 +10,35 @@ class CondaProject(ProjectSpec):
     spec_doc = "https://conda-incubator.github.io/conda-project/tutorial.html"
 
     def match(self) -> bool:
+        # TODO: a .condarc or environment.yml file is actually enough, e.g.,
+        #  https://github.com/conda-incubator/conda-project/tree/main/examples/condarc-settings
+        #  but we could argue that such are not really _useful_ projects; but can you
+        #  ever see a .condarc otherwise?
+        # $ conda-project.yml
+        # name: learn-cp
+        #   environments:
+        #     default:
+        #     - environment.yml
+        # variables: {}
+        # commands: {}
+
         basenames = {_.rsplit("/", 1)[-1] for _ in self.root.filelist}
         return not basenames.isdisjoint(
             {"conda-project.yml", "conda-meta.yaml"}
         )
 
     def parse(self) -> None:
-        # TODO: a .condarc or environment.yml file is actually enough, e.g.,
-        #  https://github.com/conda-incubator/conda-project/tree/main/examples/condarc-settings
-        #  but we could argue that such are not really _useful_ projects; but can you
-        #  ever see a .condarc otherwise?
-
+        cont = AttrDict()
+        arts = AttrDict()
         try:
             with self.root.fs.open(f"{self.root.url}/conda-project.yml") as f:
-                _yaml_no_jinja(f)
+                meta = _yaml_no_jinja(f)
         except FileNotFoundError:
             with self.root.fs.open(f"{self.root.url}/conda-project.yaml") as f:
-                _yaml_no_jinja(f)
+                meta = _yaml_no_jinja(f)
+
+        # lockfiles are called "conda-lock.<env_name>.yml"
+        # envs live in ./envs/<env_name/
+
+        self._contents = cont
+        self._artifacts = arts
