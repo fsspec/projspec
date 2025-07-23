1ce73040032f08ae7699944c068b927090d7d612
Martin Durant <martin.durant@alumni.utoronto.ca>
2025-07-23 09:59:42 -0400

    conda-project multifile envs

--- src/projspec/proj/conda_project.py
+++ src/projspec/proj/conda_project.py
@@ -3,7 +3,7 @@ import yaml
 from projspec.proj import ProjectSpec
 from projspec.utils import AttrDict, _yaml_no_jinja
 
-# lockfile format is as produced by conda-lock
+# lockfile format is as produced by conda-lock; may be a standalone thing to find?
 # https://github.com/conda/conda-lock
 
 
@@ -16,13 +16,6 @@ class CondaProject(ProjectSpec):
         #  https://github.com/conda-incubator/conda-project/tree/main/examples/condarc-settings
         #  but we could argue that such are not really _useful_ projects; but can you
         #  ever see a .condarc otherwise?
-        # $ conda-project.yml
-        # name: learn-cp
-        #   environments:
-        #     default:
-        #     - environment.yml
-        # variables: {}
-        # commands: {}
 
         basenames = {_.rsplit("/", 1)[-1] for _ in self.root.filelist}
         return not basenames.isdisjoint(
@@ -47,14 +40,28 @@ class CondaProject(ProjectSpec):
         runtimes = AttrDict()
         cmds = AttrDict()
         procs = AttrDict()
-        for env_name, fname in meta.get("environments", {}).items():
+        for env_name, fspec in meta.get("environments", {}).items():
             try:
-                if isinstance(fname, list):
-                    fname = fname[0]
-                with self.root.fs.open(f"{self.root.url}/{fname}") as f:
-                    env = _yaml_no_jinja(f)
-                    channels = env.get("channels", [])
-                    packages = env.get("dependencies", [])
+                fnames = [fspec] if isinstance(fspec, str) else fspec
+                channels = []
+                packages = []
+                for fname in fnames:
+                    with self.root.fs.open(f"{self.root.url}/{fname}") as f:
+                        env = _yaml_no_jinja(f)
+                        channels.extend(
+                            [
+                                _
+                                for _ in env.get("channels", [])
+                                if _ not in channels
+                            ]
+                        )
+                        packages.extend(
+                            [
+                                _
+                                for _ in env.get("dependencies", [])
+                                if _ not in packages
+                            ]
+                        )
                 runtime = CondaEnv(
                     proj=self.root,
                     cmd=["conda", "project", "prepare", env_name],
@@ -105,9 +112,6 @@ class CondaProject(ProjectSpec):
 
         cont = AttrDict(envs=envs, commands=cmds)
         arts = AttrDict(lock=locks, conda_env=runtimes, process=procs)
-
-        # lockfiles are called "conda-lock.<env_name>.yml"
-        # envs live in ./envs/<env_name/
 
         self._contents = cont
         self._artifacts = arts
