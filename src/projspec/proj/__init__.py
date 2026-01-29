25cded673d1c5d74c8b07c450cc55d0e2654fc1e
Martin Durant <martin.durant@alumni.utoronto.ca>
2026-01-29 12:26:55 -0500

    don't auto-import library (can be slow)

--- src/projspec/proj/__init__.py
+++ src/projspec/proj/__init__.py
@@ -12,7 +12,7 @@ from projspec.proj.pyscript import PyScript
 from projspec.proj.python_code import PythonCode, PythonLibrary
 from projspec.proj.rust import Rust, RustPython
 from projspec.proj.uv import Uv
-from projspec.proj.webapp import StreamlitApp
+from projspec.proj.webapp import Streamlit
 
 __all__ = [
     "ParseFailed",
@@ -36,6 +36,7 @@ __all__ = [
     "RTD",
     "Rust",
     "RustPython",
+    "Streamlit",
     "Uv",
     "VSCode",
     "Yarn",
--- src/projspec/proj/webapp.py
+++ src/projspec/proj/webapp.py
@@ -1,12 +1,15 @@
 from projspec.proj import ProjectSpec
 
 
-class StreamlitApp(ProjectSpec):
+class Streamlit(ProjectSpec):
+    """Interactive graphical app served in the browser, with streamlit components"""
+
     spec_doc = "https://docs.streamlit.io/deploy/streamlit-community-cloud/deploy-your-app/file-organization"
     # see also "https://docs.streamlit.io/develop/api-reference/configuration/config.toml", which is
     # mainly theme and server config.
 
     def match(self) -> bool:
+        # more possible layouts
         return bool(
             {".streamlit", "streamlit_app.py"}.intersection(self.proj.basenames)
         )
@@ -14,6 +17,8 @@ class StreamlitApp(ProjectSpec):
     def parse(self) -> None:
         from projspec.content.environment import PythonRequirements, CondaEnv
         from projspec.artifact.process import Server
+
+        # https://docs.streamlit.io/deploy/streamlit-community-cloud/deploy-your-app/app-dependencies
 
         # a requirements.txt file is the most common and suggested way, but uv, pipenv and poetry are
         # also supported - but they will get picked up as separate project types.
@@ -27,6 +32,9 @@ class StreamlitApp(ProjectSpec):
                     p.parse()
                     self._contents["environment"] = p.contents["environment"]
                     break
+        if "environment" in self.proj.contents:
+            self._contents["environment"] = self.proj.contents["environment"]
+        # TODO: packages.txt lists packages to apt-get
 
         # the common case is a single .py file
         pyfiles = [v for v in self.proj.basenames if v.endswith(".py")]
