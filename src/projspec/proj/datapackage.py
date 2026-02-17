95f04ba5188521fbdef95d4ac10e81f040f30685
Martin Durant <martin.durant@alumni.utoronto.ca>
2026-02-17 17:09:40 -0500

    Add dvc

--- src/projspec/proj/datapackage.py
+++ src/projspec/proj/datapackage.py
@@ -14,3 +14,29 @@ class DataPackage(ProjectSpec):
 
     # pythonic API
     # https://framework.frictionlessdata.io/docs/framework/actions.html
+
+
+class DVCRepo(ProjectSpec):
+    """Git management of data assets within a repo"""
+
+    spec_doc = "https://doc.dvc.org/command-reference/config"
+
+    def match(self) -> bool:
+        return ".dvc" in self.proj.basenames
+
+    def parse(self) -> None:
+        import configparser
+
+        conf = {}
+        for fn in ["config", "config.local"]:
+            # latter config wins, if both exist
+            parser = configparser.ConfigParser()
+            try:
+                with self.proj.fs.open(f"{self.proj.url}/.dvc/{fn}", "rt") as f:
+                    parser.read_file(f)
+                    conf.update(parser._sections)
+            except (IOError, ValueError):
+                pass
+        self.contents["remotes"] = [
+            _.split(" ", 1)[1][1:-2] for _ in conf if _.startswith("'remote ")
+        ]
