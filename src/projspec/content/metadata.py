f18e7673e475b00655aa0326ffb12a59ac243cab
Martin Durant <martin.durant@alumni.utoronto.ca>
2026-02-27 16:10:03 -0500

    Add a few common licenses

--- src/projspec/content/metadata.py
+++ src/projspec/content/metadata.py
@@ -52,21 +52,34 @@ class Licensed(ProjectExtra):
         text = self.proj.fs.open(self.proj.basenames[self.lic_file], "rt").read()
         lic = None
         for k, v in known.items():
-            if k in text:
+            if re.search(k, text):
                 lic = License(
                     proj=self.proj,
                     shortname=v[0],
                     fullname=v[1] or k,
-                    url=v[2],
+                    url=f"https://spdx.org/licenses/{v[0]}.html",
                 )
                 break
         self._contents["license"] = lic or License(proj=self.proj, url=self.lic_file)
 
 
+# could embed https://spdx.org/licenses/licenses.json , 330kB
+# search: (shortname, fullname)
+# if fullname is None, same as search
 known = {
-    "BSD 3-Clause License": (
-        "BSD-3-Clause",
-        None,
-        "https://opensource.org/license/bsd-3-clause",
-    )
+    # many variants of this
+    "GNU GENERAL PUBLIC LICENSE.*Version 3": (
+        "GPL-3.0-or-later",
+        "GNU General Public License v3.0 or later",
+    ),
+    # few variants
+    "GNU LESSER GENERAL PUBLIC LICENSE.*Version 3": (
+        "LGPL-3.0+",
+        "GNU Lesser General Public License v3.0 or later",
+    ),
+    # also 1-clause and 2-clause listed
+    "BSD 3-Clause License": ("BSD-3-Clause", None),
+    "MIT License": ("MIT", None),
+    # also v1.0 and v1.1 listed
+    "Apache License.*Version 2.0": ("Apache-2.0", "Apache License 2.0"),
 }
